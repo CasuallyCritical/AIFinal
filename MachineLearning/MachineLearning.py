871ef3b51a84cf63977adbd134e05deaca685a8d
CasuallyCritical <onetruejdyt@gmail.com>
2022-12-09 14:36:17 -0500

    Update MachineLearning.py

--- MachineLearning/MachineLearning.py
+++ MachineLearning/MachineLearning.py
@@ -175,12 +175,13 @@ def back_propagation(train, test, l_rate, n_epoch, n_hidden):
 	for row in test:
 		prediction = predict(network, row)
 		predictions.append(prediction)
+	print(predictions)
 	return(predictions)
 
 # Test Backprop on Seeds dataset
 seed(1)
 # load and prepare data
-filename = 'wheat-seeds.csv'
+filename = 'xordata.csv'
 dataset = load_csv(filename)
 for i in range(len(dataset[0])-1):
 	str_column_to_float(dataset, i)
@@ -190,10 +191,10 @@ str_column_to_int(dataset, len(dataset[0])-1)
 minmax = dataset_minmax(dataset)
 normalize_dataset(dataset, minmax)
 # evaluate algorithm
-n_folds = 5
+n_folds = 10
 l_rate = 0.3
-n_epoch = 500
-n_hidden = 5
+n_epoch = 1000
+n_hidden = 3
 scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
 print('Scores: %s' % scores)
 print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
